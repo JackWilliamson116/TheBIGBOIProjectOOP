769cd507e64b9a28f526d173141a1420595caaf4
JackWilliamson116 <28870092+JackWilliamson116@users.noreply.github.com>
2018-11-29 12:17:30 -0500

    Update Circuit.h

--- Circuit.h
+++ Circuit.h
@@ -10,4 +10,13 @@ public:
 	void insertWire(char c, wire* w1);
 private:
 	map<char, wire*> wireMap;
+	int pqTB;
+};
+class event {
+public:
+	event(wire w1, int time, wireValue val) : w1(w1), time(time), val(val) {};
+private:
+	wire w1;
+	int time;
+	wireValue val;
 };
