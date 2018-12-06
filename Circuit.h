8585d2ae191bfd035f8bfa4e20f3ebf7ff161c5a
Andrew-T-Smith <43500827+Andrew-T-Smith@users.noreply.github.com>
2018-12-05 22:30:34 -0500

    Update Circuit.h

--- Circuit.h
+++ Circuit.h
@@ -2,21 +2,35 @@
 #include <iostream>
 #include <map>
 #include "wire.h"
+#include "gate.h"
 
 using namespace std;
-
+class event;
 class circuit {
 public:
 	void insertWire(char c, wire* w1);
+	wire* getWire(int wnum);
+	bool doesNotExist(int wnum);
+	void createGateConnection(wire* in1, wire* in2, wire* out, int delay, gateType type);
+	event* makeEvent(wire w1, int time, wireValue val);
+	wireValue setWireValue(char c);
 private:
 	map<char, wire*> wireMap;
-	int pqTB;
+	int pqTB = 0;
+	map<char, wire*>::iterator it;
 };
 class event {
 public:
-	event(wire w1, int time, wireValue val) : w1(w1), time(time), val(val) {};
+	event(wire w1, int time, wireValue val, int pqTieBreaker) : w1(w1), time(time), val(val), pqTieBreaker(pqTieBreaker) {};
+	int getTieBreaker() { return pqTieBreaker; }
+	int getTime() { return time; }
+	wire getWire() { return w1; }
+	wireValue getVal() { return val; }
+
+
 private:
 	wire w1;
 	int time;
 	wireValue val;
+	int pqTieBreaker;
 };
